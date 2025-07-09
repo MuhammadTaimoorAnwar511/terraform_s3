aac020e0b0687692fe0b405c253ad48f4b657780
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2025-07-09 16:51:03 +0500

    second

--- s3_output.tf
+++ s3_output.tf
@@ -10,12 +10,6 @@ output "s3_bucket_website_endpoint" {
   value       = var.s3_static_web_hosting ? "http://${aws_s3_bucket_website_configuration.static[0].website_endpoint}" : ""
   description = "The S3 bucket website endpoint (only if hosting is enabled)"
 }
-
-
-# output "s3_bucket_website_endpoint" {
-#   value       = var.s3_static_web_hosting ? aws_s3_bucket_website_configuration.static[0].website_endpoint : ""
-#   description = "The S3 bucket website endpoint (only if hosting is enabled)"
-# }
 # output "s3_bucket_website_domain" {
 #   description = "The domain of the website endpoint, if the bucket is configured with a website. If not, this will be an empty string. This is used to create Route 53 alias records. "
 #   value       = var.s3_static_web_hosting ? aws_s3_bucket_website_configuration.static[0].website_domain : ""
